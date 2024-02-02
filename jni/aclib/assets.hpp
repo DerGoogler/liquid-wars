447460c4c8f448aaa14562979ec79555aef00ba1
Henry Shepperd <hshepperd@gmail.com>
2024-02-02 17:09:53 +1100

    Remove my name from copyright notice

--- jni/aclib/assets.hpp
+++ jni/aclib/assets.hpp
@@ -1,7 +1,5 @@
 //    This file is part of Liquid Wars.
 //
-//    Copyright (C) 2013 Henry Shepperd (hshepperd@gmail.com)
-//
 //    Liquid Wars is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
